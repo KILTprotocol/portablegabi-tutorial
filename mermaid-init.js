7c59df9b42d1cc4e2eabc86d1a754932bb840371
weichweich <weichweich@users.noreply.github.com>
2020-03-19 14:16:59 +0000

    deploy: c24b6c6c1f39b2d5aadb20af649fc8de880be79e

---
+++ mermaid-init.js
@@ -0,0 +1,11 @@
+mermaid.initialize({
+    startOnLoad:true,
+    sequenceConfig = {
+        diagramMarginX:50,
+        diagramMarginY:10,
+        boxTextMargin:5,
+        noteMargin:10,
+        messageMargin:35,
+        mirrorActors:false
+    }
+});
